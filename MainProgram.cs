6c2eb9b0e422fa08043519bf8f5e5c2766aed15b
AestheticalZ <51166756+AestheticalZ@users.noreply.github.com>
2021-08-10 06:49:21 +0200

    Fucking visual studio froze on creation of this repo...

---
+++ MainProgram.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using Pastel;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using NAudio.Wave;
+using System.IO;
+
+namespace VoxMicrophone
+{
+    public class MainProgram
+    {
+        public static bool DeviceSelected = false;
+        public static int PlayDelay = 0;
+        public static string SelectedVoice = "vox";
+
+        public static List<string> ValidVoices = new List<string>
+        {
+            "vox",
+            "fvox",
+            "hgrunt"
+        };
+
+        static void Main(string[] args)
+        {
+            Console.Title = "VoxMicrophone";
+            Console.WriteLine("VoxMicrophone ".Pastel(Color.Orange) + "by AestheticalZ".Pastel(Color.LightYellow));
+            Console.WriteLine("Use the " + "help".Pastel(Color.Yellow) + " command to get a list of available commands.");
+            Console.WriteLine("Use the " + "setvoice".Pastel(Color.Yellow) + " command to change the voice.");
+
+            VoxSpeaker voxSpeaker = new VoxSpeaker();
+            WaveOut waveOut = new WaveOut();
+
+            EnterCommand:
+            Console.Write("\n-> ".Pastel(Color.CornflowerBlue));
+
+            string[] command = Console.ReadLine().Trim().ToLower().Split(' ');
+            Console.WriteLine();
+            switch(command[0])
+            {
+                case "say":
+                    if (command.Length == 1)
+                    {
+                        PrintError("Not enough arguments.");
+                        goto EnterCommand;
+                    }
+
+                    if (DeviceSelected == false)
+                    {
+                        PrintError("You have not selected any device!");
+                        goto EnterCommand;
+                    }
+                    List<string> sentence = command.ToList(); sentence.RemoveAt(0);
+
+                    PrintMessage("Processing sentence...");
+                    if(voxSpeaker.ProcessText(sentence, SelectedVoice) == 1)
+                    {
+                        goto EnterCommand;
+                    }
+                    voxSpeaker.PlayQueue(ref waveOut);
+
+                    goto EnterCommand;
+
+                case "devices":
+                    Console.WriteLine(string.Concat("Do note that due to limitations with the Windows API,\n",
+                                      "names are limited to 32 characters.\n").Pastel(Color.Red));
+
+                    Console.WriteLine("------------AVAILABLE DEVICES------------".Pastel(Color.Aqua));
+                    for (int n = -1; n < WaveOut.DeviceCount; n++)
+                    {
+                        var caps = WaveOut.GetCapabilities(n);
+                        Console.WriteLine($"Device {n}: ".Pastel(Color.Aquamarine) + caps.ProductName);
+                    }
+                    Console.WriteLine("-----------------------------------------".Pastel(Color.Aqua));
+                    goto EnterCommand;
+
+                case "setvoice":
+                    if (command.Length == 1)
+                    {
+                        PrintError("Not enough arguments.");
+                        goto EnterCommand;
+                    }
+
+                    if(!ValidVoices.Contains(command[1]))
+                    {
+                        PrintError($"Voice \"{command[1]}\" does not exist.");
+                        goto EnterCommand;
+                    }
+                    SelectedVoice = command[1];
+                    PrintMessage($"Voice is now set to {command[1]}.");
+
+                    goto EnterCommand;
+
+                case "setdevice":
+                    if (command.Length == 1)
+                    {
+                        PrintError("Not enough arguments.");
+                        goto EnterCommand;
+                    }
+
+                    if (int.TryParse(command[1], out int deviceNum))
+                    {
+                        waveOut.DeviceNumber = deviceNum;
+                        PrintMessage($"Selected device is now \"{WaveOut.GetCapabilities(deviceNum).ProductName}\".");
+                        DeviceSelected = true;
+                    }
+                    else PrintError("Please provide a correct device number.");
+                    goto EnterCommand;
+
+                case "setdelay":
+                    if(command.Length == 1)
+                    {
+                        PrintError("Not enough arguments.");
+                        goto EnterCommand;
+                    }
+
+                    if (int.TryParse(command[1], out int delay))
+                    {
+                        PlayDelay = delay;
+                        PrintMessage($"Delay is now {delay}.");
+                    }
+                    else PrintError("Please provide a correct delay.");
+                    goto EnterCommand;
+
+                case "clear":
+                    Console.Clear();
+                    Console.WriteLine("VoxMicrophone ".Pastel(Color.Orange)
+                        + "by AestheticalZ".Pastel(Color.LightYellow));
+                    Console.WriteLine("Use the " + "help".Pastel(Color.Yellow) + " command to get a list of available commands.");
+                    Console.WriteLine("Use the " + "setvoice".Pastel(Color.Yellow) + " command to change the voice.");
+
+                    goto EnterCommand;
+
+                case "help":
+                    PrintHelp();
+                    goto EnterCommand;
+
+                default:
+                    PrintError("Unknown command.");
+                    goto EnterCommand;
+            }
+        }
+
+        public static void PrintHelp()
+        {
+            Console.WriteLine("------------AVAILABLE COMMANDS------------".Pastel(Color.Aqua));
+
+            Console.Write("say <sentence> : ".Pastel(Color.CornflowerBlue));
+            Console.WriteLine("Says a sentence to the selected device.");
+
+            Console.Write("devices : ".Pastel(Color.CornflowerBlue));
+            Console.WriteLine("Lists available devices and their number IDs.");
+
+            Console.Write("setdevice <number> : ".Pastel(Color.CornflowerBlue));
+            Console.WriteLine("Sets a device ID as the currently selected device.");
+
+            Console.Write("setdelay <seconds> : ".Pastel(Color.CornflowerBlue));
+            Console.WriteLine("Sets a delay before the sentence plays.");
+
+            Console.Write("clear : ".Pastel(Color.CornflowerBlue));
+            Console.WriteLine("Clears the console output.");
+
+            Console.Write("setvoice <voice> : ".Pastel(Color.CornflowerBlue));
+            Console.WriteLine("Sets the voice. Voice types are vox, fvox and hgrunt.");
+
+            Console.Write("help : ".Pastel(Color.CornflowerBlue));
+            Console.WriteLine("This command.");
+            Console.WriteLine("------------------------------------------".Pastel(Color.Aqua));
+        }
+
+        public static void PrintMessage(string message)
+        {
+            Console.WriteLine("[MESSAGE] ".Pastel(Color.CadetBlue) + message);
+        }
+
+        public static void PrintError(string message)
+        {
+            Console.WriteLine("[ERROR] ".Pastel(Color.IndianRed) + message);
+        }
+    }
+}
---
+++ VoxSpeaker.cs
@@ -0,0 +1,88 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace VoxMicrophone
+{
+    public class VoxSpeaker
+    {
+        public List<string> WaveQueue = new List<string>();
+
+        public int ProcessText(List<string> text, string selectedVoice)
+        {
+            foreach (string token in text)
+            {
+                if (!File.Exists($"{selectedVoice}\\{token}.wav"))
+                {
+                    MainProgram.PrintError($"Word \"{token}.wav\" does not exist.");
+                    WaveQueue.Clear();
+                    return 1;
+                }
+                else WaveQueue.Add($"{selectedVoice}\\{token}.wav");
+            }
+            return 0;
+        }
+
+        public void PlayQueue(ref WaveOut waveOut)
+        {
+            byte[] buffer = new byte[1024];
+            MemoryStream stream = new MemoryStream();
+
+            WaveFileWriter writer = null;
+            WaveFileReader reader = null;
+
+            foreach (string word in WaveQueue)
+            {
+                try
+                {
+                    reader = new WaveFileReader(word);
+                    if (writer == null) writer = new WaveFileWriter(stream, reader.WaveFormat);
+
+                    if (!reader.WaveFormat.Equals(writer.WaveFormat))
+                    {
+                        MainProgram.PrintError("One of the WAVE files has a different format.");
+                        WaveQueue.Clear();
+                        return;
+                    }
+
+                    int read = 0;
+                    while ((read = reader.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        writer.Write(buffer, 0, read);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MainProgram.PrintError(ex.Message);
+                }
+            }
+
+            writer.Flush();
+            reader.Dispose();
+
+            if (MainProgram.PlayDelay != 0)
+            {
+                MainProgram.PrintMessage($"Waiting {MainProgram.PlayDelay} second/s before playing...");
+                Thread.Sleep(TimeSpan.FromSeconds(MainProgram.PlayDelay));
+            }
+            MainProgram.PrintMessage("Playing...");
+
+            stream.Seek(0, SeekOrigin.Begin);
+            using (WaveFileReader newReader = new WaveFileReader(stream))
+            {
+                waveOut.Init(newReader);
+                waveOut.Play();
+                while (waveOut.PlaybackState != PlaybackState.Stopped)
+                {
+                    Thread.Sleep(500);
+                }
+            }
+            writer.Dispose();
+            stream.Dispose();
+
+            WaveQueue.Clear();
+        }
+    }
+}
